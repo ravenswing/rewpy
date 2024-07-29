81951c43290445fcf9d76e458c3c73c8aa20a9f4
Rhys Evans <rhys.al.evans@gmail.com>
2024-07-29 14:23:21 +0200

    added fes file reader

--- src/io.py
+++ src/io.py
@@ -1,14 +1,38 @@
 import pandas as pd
-from glob import glob
+import numpy as np
+from pathlib import Path
 
 
-def find_fes_files(fes_prefix: str):
-    print(fes_prefix)
+def find_fes_files(fes_prefix: str) -> list:
+    path = Path(fes_prefix)
+    # Extract the numbers of the fes files present, with the allocated name.
+    fes_numbers = [
+        str(a.stem).split(str(path.name))[-1]
+        for a in path.parent.rglob(str(path.name) + "*")
+    ]
+    # Check that all the fes files found fit the naming convention, and numbers can be parsed as int.
+    assert all(
+        [s.isdigit() for s in fes_numbers]
+    ), "Some FES filenames do not conform to the required numbering of <pref><num>.dat"
+    # Convert the file numbers to integers and extract the max and min number.
+    fes_numbers = np.asarray([int(s) for s in fes_numbers])
+    fes_start: int = fes_numbers.min()
+    fes_end: int = fes_numbers.max()
+    # Generate list of expected paths, with contiunous numbering and correct names.
+    fes_paths = [
+        path.with_name(f"{path.name}{i}.dat") for i in np.arange(fes_start, fes_end + 1)
+    ]
+    # Check that all of the paths exist and can be read.
+    assert all(
+        [p.is_file() for p in fes_paths]
+    ), f"Some FES files are incorrectly named or missing, expected continuous numbering from {fes_paths[0]} to {fes_paths[-1]}"
+    # Return correct, ordered, readable list of paths.
+    return fes_paths
 
 
-def load_fes(filename: str) -> pd.DataFrame:
+def load_fes(path) -> pd.DataFrame:
     # Read in the header line of the FES file.
-    with open(filename) as f:
+    with open(path) as f:
         header = f.readlines()[0].split()
     # Sum_hills FES contain FIELDS line, so simply extract the column names.
     assert header[0] == "#!", "Header not found in FES file, cannot read column names"
@@ -18,7 +42,7 @@ def load_fes(filename: str) -> pd.DataFrame:
     fields = [f.replace("file.free", "free") for f in fields]
     # If needed... Extract CV names - all column names before the free energy.
     # cvs = fields[: fields.index("free")]
-    fes = pd.read_csv(filename, sep="\s+", names=fields, comment="#")
+    fes = pd.read_csv(path, sep="\s+", names=fields, comment="#")
 
     return fes
 
